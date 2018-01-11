0b15547c88ac2f710f8751c6749709c17e104dc0
筱枫 <q849958241@163.com>
2018-01-11 15:46:03 +0800

    搭建框架

---
+++ laravel/app/Providers/SecretProvider.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SecretProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('secret', function($app) {
+            return new secret();
+        });
+    }
+}
+
+class secret
+{
+    private $openssl;
+    private $method = 'AES-256-CFB';        // 具体支持的算法可以用openssl_get_cipher_methods取得
+    private $key = '';
+
+    /**
+     * secret constructor.
+     * @param string $key
+     * @throws \Exception
+     */
+    public function __construct($key = '123456789')
+    {
+        $this->setKey($key);
+
+        if (!in_array($this->method, openssl_get_cipher_methods()))
+            throw new \Exception("encrypt method unsuporise!");
+    }
+
+    /**
+     * @param $data
+     * @param string $key
+     * @return string
+     */
+    public function encrypt($data, $key = '')
+    {
+        if (empty($key))
+            $key = $this->key;
+
+        return openssl_encrypt($data, $this->method, $key, OPENSSL_RAW_DATA, substr(md5($key), 0, 16));
+    }
+
+    /**
+     * @param $data
+     * @param string $key
+     * @return string
+     */
+    public function decrypt($data, $key = '')
+    {
+        if (empty($key))
+            $key = $this->key;
+
+        return openssl_decrypt($data, $this->method, $key, OPENSSL_RAW_DATA, substr(md5($key), 0, 16));
+    }
+
+    /**
+     * @param $key
+     * @throws \Exception
+     */
+    public function setKey($key)
+    {
+        if (empty($key))
+            throw new \Exception("key don't is null!");
+
+        $this->$key = $key;
+    }
+}
