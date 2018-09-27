7832c6ea8d8ed0c5ac7f66be3abe7b79a9d87593
筱枫 <q849958241@163.com>
2018-09-27 15:11:06 +0800

    修正api权限刷新token时不验证token已过期

--- laravel/app/Http/Middleware/JWTAuth.php
+++ laravel/app/Http/Middleware/JWTAuth.php
@@ -24,7 +24,9 @@ class JWTAuth extends BaseMiddleware
                 return apiJson([], '验证失败', 103, 401);
 
         } catch (TokenExpiredException $e) {
-            return apiJson([], 'token已过期', 101, 401);
+            // 刷新token的情况下不需要验证是否已过期
+            if (false === strpos($request->url(), 'api/refresh'))
+                return apiJson([], 'token已过期', 101, 401);
         } catch (TokenInvalidException $e) {
             return apiJson([],  'token无效', 102, 401);
         } catch (\Exception $e){
